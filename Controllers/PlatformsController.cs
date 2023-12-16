0281862d7f6e9aa030958339312d4b3cdbf73071
jad-Gh <jadghalayini1999@gmail.com>
2023-12-16 13:55:24 +0200

    Add project files.

---
+++ Controllers/PlatformsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommandService.Controllers
+{
+    [ApiController]
+    [Route("api/c/[controller]")]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        {
+            
+        }
+
+        [HttpPost]
+        public ActionResult TestInboundConnection() 
+        {
+            Console.WriteLine("--> Inbound POST # Command Service");
+            return Ok("Inbound test of platforms Controller");
+        }
+    }
+}
